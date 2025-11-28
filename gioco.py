620d98c894c19fd712b01c08ff99b647c74b4e8d
mbou-2025 <mohamed.boussaid@itis.biella.it>
2025-11-28 12:38:10 +0100

    Add files via upload

---
+++ gioco.py
@@ -0,0 +1,71 @@
+from pgzero.actor import Actor
+
+from pgzero.clock import clock
+
+from random import randint
+
+import pgzrun
+
+TITLE = "Colpisci l'alieno"
+
+WIDTH = 800
+
+HEIGHT = 600
+ 
+messaggio = ""
+ 
+alieno = Actor("alieno")
+ 
+def draw():
+
+    screen.clear()
+
+    screen.fill(color=(104, 104, 104))
+
+    alieno.draw()
+
+    screen.draw.text(messaggio, center=(400, 40), fontsize=60)
+ 
+def piazza_alieno():
+
+    '''
+
+    Il limite di 50 pixel è definito per evitare che l'immagine 
+
+    sia parzialmente fuori schermo
+
+    Alieno ha size 64x64
+
+    '''
+
+    alieno.x = randint(50, WIDTH-50)
+
+    alieno.y = randint(50, HEIGHT-50)
+
+    alieno.image = "alieno"
+ 
+ 
+def on_mouse_down(pos):
+
+    global messaggio
+
+    if alieno.collidepoint(pos):
+
+        messaggio = "Bel colpo!"
+
+        alieno.image = "esplosione"
+        
+        sounds.colpo.play()
+
+    else:
+
+        messaggio = "Mancato..."
+ 
+ 
+piazza_alieno()
+
+clock.schedule_interval(piazza_alieno, 0.7)
+
+pgzrun.go()
+ 
+ 
